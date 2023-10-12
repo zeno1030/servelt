e04916055265dc20a6461fef5bda9a8a0b7b526c
ZENO <zeno1030@naver.com>
2023-10-12 14:57:24 +0900

    Initial commit

---
+++ src/main/java/hello/servelt/ServeltApplication.java
@@ -0,0 +1,13 @@
+package hello.servelt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/servelt/ServeltApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.servelt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
