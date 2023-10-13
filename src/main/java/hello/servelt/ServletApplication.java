83d8e5567e84baf6556832bfb77bf398dd55ef4d
ZENO <zeno1030@naver.com>
2023-10-13 16:58:11 +0900

    sevlet

--- src/main/java/hello/servelt/ServletApplication.java
+++ src/main/java/hello/servelt/ServletApplication.java
@@ -2,12 +2,14 @@ package hello.servelt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
-public class ServeltApplication {
+public class ServletApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServeltApplication.class, args);
+		SpringApplication.run(ServletApplication.class, args);
 	}
 
 }
--- src/main/java/hello/servelt/ServletInitializer.java
+++ src/main/java/hello/servelt/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ServeltApplication.class);
+		return application.sources(ServletApplication.class);
 	}
 
 }
