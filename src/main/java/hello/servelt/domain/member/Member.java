1743a0517f8d84f5539230d058b342c0ce83b1b6
ZENO <zeno1030@naver.com>
2023-10-18 11:39:26 +0900

    Test오류 해결해야함

---
+++ src/main/java/hello/servelt/domain/member/Member.java
@@ -0,0 +1,19 @@
+package hello.servelt.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Member {
+    private Long id;
+    private String username;
+    private int age;
+
+    //기본 생성자
+    public Member() {
+    }
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}
---
+++ src/test/java/hello/servelt/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package hello.servelt.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+    @AfterEach
+    void afterEach(){
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save(){
+        Member member = new Member("hello", 20);
+        Member savedMember = memberRepository.save(member);
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+
+    }
+
+    @Test
+    void findAll(){
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        List<Member> result = memberRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
